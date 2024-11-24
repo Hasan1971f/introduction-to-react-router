dc3f6d9c6e1cf4262e11c53cfb1a0f76e7825819
Hasan <mdhasanurrahmanhasan101@gmail.com>
2024-11-24 11:31:42 +0600

    added a Nested Route Fantastic and Gentle Users and use Link, Outlet

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,17 @@
-
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
         <div>
-            <h2>NavBar</h2>
+           
             <nav >
-                <a href="/">Home</a>
-                <a href="/about">About</a>
-                <a href="/">Contract us</a>
+                <span>My Website</span>
+                <Link to="/">Home</Link>
+                <Link to="/users">Users</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contract">Contract</Link>
+              
             </nav>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
 
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import Home from './Components/Home/Home.jsx';
+import App from './App.jsx'
 import About from './Components/About/About.jsx';
 import Contract from './Components/Contract/Contract.jsx';
+import Users from './Components/Users/Users.jsx';
 
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home></Home>,
-    children:[
+    children: [
       {
         path: '/about',
-        element:<About></About>
+        element: <About></About>
       },
       {
-        path: 'contract',
+        path: '/contract',
         element: <Contract></Contract>
+      },
+      {
+        path: '/users',
+        element: <Users></Users>
       }
     ]
   },
