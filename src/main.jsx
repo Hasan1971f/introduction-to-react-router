cbdc3a67815c524c286ff9ffbd4f2c2ce31fab04
Hasan <mdhasanurrahmanhasan101@gmail.com>
2024-11-24 22:25:28 +0600

    added a (Recap) Dynamic Route, params and load dynamic data

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,9 @@ import About from './Components/About/About.jsx';
 import Contract from './Components/Contract/Contract.jsx';
 import Users from './Components/Users/Users.jsx';
 import UserDetails from './Components/UserDetails/UserDetails.jsx';
+import Posts from './Components/Posts/Posts.jsx';
+import PostDetails from './Components/PostDetails/PostDetails.jsx';
+
 
 
 const router = createBrowserRouter([
@@ -37,7 +40,18 @@ const router = createBrowserRouter([
         path: '/user/:userId',
         loader: ({params})=> fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
         element: <UserDetails></UserDetails>
+      },{
+        path: '/posts',
+        loader: ()=> fetch('https://jsonplaceholder.typicode.com/posts'),
+        element: <Posts></Posts>
+      
+      },
+      {
+        path: '/post/:postId',
+        loader: ({params})=> fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`),
+        element: <PostDetails></PostDetails>
       }
+      
     ]
   },
   
